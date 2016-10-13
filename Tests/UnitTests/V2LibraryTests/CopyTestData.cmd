86cf40ba139e32ce904fc3d9b6a3b8f46d4046a2
KeDengMS <kedeng@microsoft.com>
2016-10-13 15:53:11 -0700

    Remove the script, just point working directory to data folder

--- Tests/UnitTests/V2LibraryTests/CopyTestData.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-set local
-set TESTROOT=..\..
-xcopy /d %TESTROOT%\EndToEndTests\Text\SequenceClassification\Data\Train.ctf .
-xcopy /d %TESTROOT%\EndToEndTests\Speech\Data\SimpleData*.txt .
